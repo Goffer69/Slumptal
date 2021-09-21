0d3ccb40c4e8319f18b6272da5fee7a9e2e16cf3
Goffer69 <91138373+Goffer69@users.noreply.github.com>
2021-09-21 16:08:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Wilmer_Wallentin
+{
+    class Program
+    {
+        static void Main(string[] args)
+       
+        {
+            int antalTal, räknare = 1;
+            double summa 0, input;
+            antalTal = int.Parse(Console.ReadLine())
+
+            Random slump =new Random();
+            slump.Next(1,1000);
+            Console.WriteLine ("0:"+slump.Next(500));
+
+
+        }
+    }
+}
